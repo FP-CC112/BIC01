7b1f1353d216b9da885ff3037b2e7f835e8d5df1
AmericoCh <americochulluncuy@gmail.com>
2025-04-29 07:59:49 -0500

    Lab06

---
+++ Lab06/fibonacci.cpp
@@ -0,0 +1,30 @@
+﻿/*Escribe un programa que imprima los N primeros
+términos de la sucesión
+a = 0;
+b = 1;
+actual = a + b;
+a = b;
+b = actual;
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cout << "Ingrese la cantidad de términos: ";
+    cin >> N;
+    int a = 0;
+    int b = 1;
+    int actual;
+
+    for(int i = 0; i < N; ++i){
+        cout << a << "\t";
+        actual = a + b;
+        a = b;
+        b = actual;
+    }
+
+
+    return 0; 
+}
