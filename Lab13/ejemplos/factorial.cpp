9bc419d0b02ee85b718937a12be0e31978cae993
FP-CC112 <chreyaman_u27@hotmail.com>
2025-06-17 08:11:41 -0500

    pd13

---
+++ Lab13/ejemplos/factorial.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int factorial(int n) {
+    int resultado = 1;
+    for(int i = 2; i <= n; ++i) {
+        resultado *= i;
+    }
+    return resultado;
+}
+
+int factorialRec(int n) {
+    //Caso base
+    if (n == 1 || n == 0)
+        return 1;
+    //Caso recursivo
+    return n * factorialRec(n-1);
+}
+
+int main(){
+    cout << factorial(10) <<endl;
+    cout << factorial(10) << endl;
+
+    return 0; 
+}
---
+++ Lab13/ejemplos/fibonacci.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n == 0 || n ==1)
+        return n;
+
+    int a = 0, b = 1, temp;
+    for (int i = 2; i <=n; ++i) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
+}
+
+
+int fibonacciRec(int n) {
+    //Caso base
+    if (n == 0 || n == 1)
+        return n;
+
+    //Caso recursivo
+    return fibonacciRec(n-1) + fibonacciRec(n-2);
+}
+int main(){
+
+    cout << fibonacci(8) <<endl;
+    cout << fibonacciRec(8) <<endl;
+
+    return 0; 
+}
