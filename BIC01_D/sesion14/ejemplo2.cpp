fb502515056a255621c4bfe89683c0037b7020c2
FP-CC112 <chreyaman_u27@hotmail.com>
2025-06-26 07:01:50 -0500

    ejemplos sesion14

---
+++ BIC01_D/sesion14/ejemplo2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstring> // para utilizar funciones con cadenas al estilo C
+#include <string> // para manejar cadenas al estilo C++
+using namespace std;
+
+int main(){
+    // Lectura de cadenas al estilo C
+    char nombres[50];
+    cout << "Ingrese sus nombres: ";
+    cin.getline(nombres, 50); 
+    cout << "Hola " << nombres <<endl;
+
+    // // Lectura de cadenas al estilo C++
+    // string cadena("Juan Perez");
+    // cout << "Hola " << cadena <<endl;
+
+    return 0; 
+}
